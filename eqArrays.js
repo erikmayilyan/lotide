9a729aecceed23c75cbd42df34109f3dfb1c7986
Mrdo Mrdo <mayilyane9@gmail.com>
2021-08-17 17:03:35 -0700

    Commit eqArrays

---
+++ eqArrays.js
@@ -0,0 +1,24 @@
+const assertEqual = function(actual, expected) {
+    if (actual === expected) {
+      return `Assertion Passed: ${actual} === ${expected}`
+    }
+    return `Assertion Failed: ${actual} !== ${expected}`
+};
+
+const eqArrays = function(one, two) {
+    for (let i = 0; i < one.length; i++){
+    	if (one[i] !== two[i]) {
+      	return false;
+    	}
+    }
+    return true;
+}
+  
+// TEST CODE
+console.log(eqArrays([1, 2, 3], [1, 2, 3]));
+console.log(eqArrays([1, 2, 3], [3, 2, 1]));
+
+console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"]));
+console.log(eqArrays(["1", "2", "3"], ["1", "2", 3]));
+
+console.log(assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true));
