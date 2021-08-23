0936859165be6e731839de9f64d1bc02c5a6e73c
Erik <68757973+erikmayilyan@users.noreply.github.com>
2021-08-23 14:30:38 -0700

    Update head.js

--- head.js
+++ head.js
@@ -1,11 +1,11 @@
-const assertEqual = function(actual, expected) {
-  console.log(actual === expected);
-};
+const assertEqual = require('./assertEqual');
 
 const head = function(list) {
 	return list[0];
 }
 
 // TEST CODE
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+console.log(assertEqual(head([5,6,7]), 5));
+console.log(assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello"));
+
+module.exports = head;
