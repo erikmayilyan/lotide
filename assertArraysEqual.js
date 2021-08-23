2d21aa1f841a814c541b204628f50ac6a8494490
Mrdo Mrdo <mayilyane9@gmail.com>
2021-08-23 16:32:29 -0700

    Modifying assertArraysEqual js

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,3 +1,5 @@
+const eqArrays = require('./eqArrays');
+
 const assertArraysEqual = function(first, second) {
     for (let i = 0; i < first.length; i++){
     	if (first[i] !== second[i]) {
@@ -8,3 +10,5 @@ const assertArraysEqual = function(first, second) {
 }
 
 console.log(assertArraysEqual([1, 2, 3], [1, 2, 3]));
+
+module.exports = assertArraysEqual;
