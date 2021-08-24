287aa3ebfca75c824f4d3588848b8de2b8f73b3c
Mrdo Mrdo <mayilyane9@gmail.com>
2021-08-23 17:24:27 -0700

    Modify Middle test

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,8 +1,25 @@
 const middle = require('../middle');
-const assertArraysEqual = require('../assertArraysEqual');
+const assert = require('chai').assert;
 
-console.log(assertArraysEqual(middle([1, 2, 3, 4, 5])));
-console.log(assertArraysEqual(middle([1, 2, 3, 4])));
-console.log(assertArraysEqual(middle([1, 2, 3, 4, 5, 6])));
-console.log(assertArraysEqual(middle([1])));
-console.log(assertArraysEqual(middle([1, 2])));
+describe("#middle", () => {
+  it("returns 3 in an array for [1, 2, 3, 4, 5]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5]), [3]);
+  });
+
+  it("returns 2 & 3 in an array for [1, 2, 3, 4]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4]), [2, 3]);
+  });
+
+  it("returns 3 & 4 in an array for [1, 2, 3, 4, 5, 6]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+  });
+
+  it("returns empty in an array for [1]", () => {
+    assert.deepEqual(middle([1]), []);
+  });
+
+  it("returns empty in an array for [1, 2]", () => {
+    assert.deepEqual(middle([1, 2]), []);
+  });
+});
+
